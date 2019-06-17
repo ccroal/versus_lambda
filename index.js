ba49e9749ecfb99c3f005a87ab70326aabb282ce
Chris Croal <ccroal@ntlworld.com>
2019-06-17 15:36:00 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const ApiBuilder = require('claudia-api-builder'),
+AWS = require('aws-sdk');
+const api = new ApiBuilder();
+const axios = require('axios');
+dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+api.get('/', function (req) {
+    return new Promise((resolve, resject) => {
+      try{
+            const {API_KEY} = process.env;
+
+            const url = `https://apifootball.com/api/?APIkey=${API_KEY}`
+
+            axios
+
+                  .get(url)
+                  .then(function(res) {
+                    resolve(res.data);
+                  })
+                  .catch(function(axiosError) {
+                    reject({axiosError})
+                  });
+
+          } catch (trycatchError){
+                reject({trycatchError});
+          }
+    })
+});
+
+
+module.exports = api;
